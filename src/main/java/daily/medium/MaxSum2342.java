9726b944c8a56214f2dee64397ef00012ae07962
rkm09 <raksha.mishra09@gmail.com>
2025-02-12 17:33:15 +0530

    max sum

---
+++ src/main/java/daily/medium/MaxSum2342.java
@@ -0,0 +1,54 @@
+package daily.medium;
+
+import java.util.*;
+
+public class MaxSum2342 {
+    public static void main(String[] args) {
+        int[] nums = {18,43,36,13,7};
+        int[] nums1 = {368,369,307,304,384,138,90,279,35,396,114,328,251,364,300,191,438,467,183};
+        System.out.println(maximumSum(nums1));
+    }
+
+//    def; map;
+    public static int maximumSum(int[] nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int num : nums) {
+            int curr = num, sum = 0;
+            while(curr != 0) {
+                int rdr = curr % 10;
+                sum += rdr;
+                curr /= 10;
+            }
+            map.computeIfAbsent(sum, k -> new ArrayList<>()).add(num);
+        }
+        int maxSum = -1;
+        for(int key : map.keySet()) {
+            List<Integer> numList = map.get(key);
+            if(numList.size() > 1) {
+                numList.sort((a,b) -> b - a);
+                maxSum = Math.max(maxSum, numList.get(0) + numList.get(1));
+            }
+        }
+        return maxSum;
+    }
+}
+
+/*
+You are given a 0-indexed array nums consisting of positive integers. You can choose two indices i and j, such that i != j, and the sum of digits of the number nums[i] is equal to that of nums[j].
+Return the maximum value of nums[i] + nums[j] that you can obtain over all possible indices i and j that satisfy the conditions.
+Example 1:
+Input: nums = [18,43,36,13,7]
+Output: 54
+Explanation: The pairs (i, j) that satisfy the conditions are:
+- (0, 2), both numbers have a sum of digits equal to 9, and their sum is 18 + 36 = 54.
+- (1, 4), both numbers have a sum of digits equal to 7, and their sum is 43 + 7 = 50.
+So the maximum sum that we can obtain is 54.
+Example 2:
+Input: nums = [10,12,19,14]
+Output: -1
+Explanation: There are no two numbers that satisfy the conditions, so we return -1.
+
+Constraints:
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+ */
