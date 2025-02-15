faa5e526baf25261a9912ad115022845f6afd584
rkm09 <raksha.mishra09@gmail.com>
2025-02-15 17:07:42 +0530

    valid tree

--- src/main/java/daily/medium/ValidTree261.java
+++ src/main/java/daily/medium/ValidTree261.java
@@ -63,6 +63,103 @@ public class ValidTree261 {
         return true;
     }
 
+//    advanced graph theory [there should be exactly n - 1 edges]; time: O(n), space: O(v + e)
+//    bfs [for iterative dfs replace with stack]
+    public boolean validTree2(int n, int[][] edges) {
+        if(edges.length != n - 1) return false;
+        Map<Integer, List<Integer>> adjMap = new HashMap<>();
+        for(int[] edge : edges) {
+            adjMap.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            adjMap.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+        }
+        Set<Integer> seen = new HashSet<>();
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(0);
+        seen.add(0);
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            for(int neighbour : adjMap.getOrDefault(node, new ArrayList<>())) {
+//                trivial cycle [just continue]
+                if(seen.contains(neighbour)) continue;
+                seen.add(neighbour);
+                queue.offer(neighbour);
+            }
+        }
+        return seen.size() == n;
+    }
+
+//    recursive dfs;
+    public boolean validTree3(int n, int[][] edges) {
+        if(edges.length != n - 1) return false;
+        Map<Integer, List<Integer>> adjMap = new HashMap<>();
+        for(int[] edge : edges) {
+            adjMap.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            adjMap.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+        }
+        Set<Integer> seen = new HashSet<>();
+        dfsHelper(0, adjMap, seen);
+        return seen.size() == n;
+    }
+
+    private void dfsHelper(int node, Map<Integer, List<Integer>> adjMap, Set<Integer> seen) {
+//        ignore trivial cycles and return
+        if(seen.contains(node)) return;
+        seen.add(node);
+        for(int neighbour : adjMap.getOrDefault(node, new ArrayList<>())) {
+            dfsHelper(neighbour, adjMap, seen);
+        }
+    }
+
+//    union find; time: O(n(alphaN)), space: O(n)   [fastest!]
+    public boolean validTree4(int n, int[][] edges) {
+        if(edges.length != n - 1) return false;
+        UnionFind uf = new UnionFind(n);
+        for(int[] edge : edges) {
+            uf.union(edge[0], edge[1]);
+        }
+        return uf.getCount() == 1;
+    }
+
+    class UnionFind {
+        private int[] parent;
+        private int[] rank;
+        private int count;
+
+        UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        private int find(int i) {
+            if(parent[i] != i)
+                parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        private void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if(rootX != rootY) {
+                if(rank[rootX] > rank[rootY])
+                    parent[rootY] = rootX;
+                else if(rank[rootY] > rank[rootX])
+                    parent[rootX] = rootY;
+                else {
+                    parent[rootY] = rootX;
+                    rank[rootX]++;
+                }
+                count--;
+            }
+        }
+
+        private int getCount() {
+            return count;
+        }
+    }
 }
 
 /*
@@ -87,6 +184,7 @@ Recall that a graph, G, is a tree iff the following two conditions are met:
 G is fully connected. In other words, for every pair of nodes in G, there is a path between them.
 G contains no cycles. In other words, there is exactly one path between each pair of nodes in G.
 Recall that breadth-first search and depth-first search are almost the same "algorithm", just with a different data structure.
+Basic:
 Time Complexity : O(N+E).
 Creating the adjacency list requires initialising a list of length N, with a cost of O(N), and then iterating over and inserting E edges, for a cost of O(E). This gives us O(E)+O(N)=O(N+E).
 Each node is added to the data structure once. This means that the outer loop will run N times. For each of the N nodes, its adjacent edges is iterated over once. In total, this means that all E edges are iterated over once by the inner loop. This, therefore, gives a total time complexity of O(N+E).
@@ -95,4 +193,15 @@ Space Complexity : O(N+E).
 The adjacency list is a list of length N, with inner lists with lengths that add to a total of E. This gives a total of O(N+E) space.
 In the worst case, the stack/ queue will have all N nodes on it at the same time, giving a total of O(N) space.
 In total, this gives us O(E+N) space.
+Advanced(n-1):
+Time Complexity : O(N).
+When E != N−1, we simply return false. Therefore, the worst case is when E=N−1. Because E is proportional to N, we'll say E=N to simplify the analysis.
+As said above, creating an adjacency list has a time complexity of O(N+E). Because E is now bounded by N, we can reduce this slightly to O(N+N)=O(N).
+The iterative breadth-first search and depth-first search are almost identical. Each node is put onto the queue/stack once, ensured by the seen set. Therefore, the inner "neighbour" loop runs once for each node. Across all nodes, the number of cycles this loop does is the same as the number of edges, which is simply N. Therefore, these two algorithms have a time complexity of O(N).
+UnionFind:
+Time Complexity : O(N⋅α(N)).
+When E != N−1, we simply return false. Therefore, the worst case is when E=N−1. Because E is proportional to N, we'll say E=N to simplify the analysis.
+We are putting each of the N edges into the UnionFind data structure, using the union(...) method. The union(...) method itself has no loops or recursion, so the entire cost of calling it is dependent on the cost of the find(...) method that it calls.
+find(...)'s cost is dependent on how far the node it was searching for is from the root. Using the naïve implementation of union find, this depth could be N. If this was the case for all the calls, we'd have a final cost of O(N^2).
+However, remember those optimizations we did? Those keep the tree depths very shallow. It turns out that find(...) amortizes to O(α(N)), where α is the Inverse Ackermann Function. The incredible thing about this function is that it grows so slowly that N will never go higher than 4 in the universe as we know it! So while in "practice" it is effectively O(1), in "theory" it is not.
  */
