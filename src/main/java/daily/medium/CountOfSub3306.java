8e0f7e62e3fea62f8b9a338b589e70724995082b
rkm09 <raksha.mishra09@gmail.com>
2025-03-11 13:48:17 +0530

    count of sub

--- src/main/java/daily/medium/CountOfSub3306.java
+++ src/main/java/daily/medium/CountOfSub3306.java
@@ -8,7 +8,7 @@ public class CountOfSub3306 {
         CountOfSub3306 c = new CountOfSub3306();
         String word1 = "eoeaui"; // k = 0, r - 2
         String word = "ieaouqqieaouqq"; // k = 1, r - 3
-        System.out.println(c.countOfSubstrings(word, 1));
+        System.out.println(c.countOfSubstrings1(word, 1));
     }
 
 //    sliding window; time: O(n), space: O(n)
@@ -69,6 +69,39 @@ public class CountOfSub3306 {
         return numValidSubstrings;
     }
 
+
+//    sliding window (relaxed constraints); time: O(n), space: O(n)
+    public long countOfSubstrings1(String word, int k) {
+        return atleastK(word, k) - atleastK(word, k + 1);
+    }
+
+    private long atleastK(String word, int k) {
+        int start = 0, end = 0, consonants = 0;
+        int n = word.length();
+        long numOfSubstrings = 0;
+        Map<Character, Integer> vowelCount = new HashMap<>();
+        while(end < n) {
+            char newCharacter = word.charAt(end);
+            if(isVowel(newCharacter))
+                vowelCount.put(newCharacter, vowelCount.getOrDefault(newCharacter, 0) + 1);
+            else
+                consonants++;
+            while(vowelCount.size() >= 5 && consonants >= k) {
+                numOfSubstrings += n - end;
+                char startCharacter = word.charAt(start);
+                if(isVowel(startCharacter)) {
+                    vowelCount.put(startCharacter, vowelCount.get(startCharacter) - 1);
+                    if(vowelCount.get(startCharacter) == 0)
+                        vowelCount.remove(startCharacter);
+                } else
+                    consonants--;
+                start++;
+            }
+            end++;
+        }
+        return numOfSubstrings;
+    }
+
     private boolean isVowel(Character c) {
         return (c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u');
     }
@@ -104,5 +137,17 @@ word consists only of lowercase English letters.
 
 /*
 In summary, when we come across a valid window, we can keep shrinking while the window is still valid. For each shrunken window, we have nextConsonant - end more valid windows.
-
+Relaxed constraints:
+In the previous approach, we adjusted our sliding window by strictly following the 2 constraints:
+A valid window must contain all vowels.
+A valid window must contain exactly k consonants.
+The second requirement introduces more complexity to our sliding window solution, leading us to precompute a nextConsonant array to keep track of when the next consonant occurs for all indices in the string. To simplify the problem, let's relax this second constraint, so that valid substrings have at least k consonants instead.
+Let’s say we find a window (substring) that contains all vowels and exactly k consonants. What happens if we keep expanding the window to the right?
+Adding more characters will never remove a vowel from the window.
+It may add more consonants, but since we only need at least k, the window remains valid.
+This means that once we reach our first valid window (where end is the right boundary of the window), every substring that extends from this point onward is also valid. Instead of checking each one individually, we can instantly count them:
+New valid substrings=word.length−end
+After counting these substrings, we shrink the window from the left (start index) and repeat the process, making sure our window remains valid.
+Now, the question is how we can connect this relaxed version of the problem back to the original problem. Let's denote the solution to this relaxed problem with a given word and k as atLeastK(word, k). The key observation is the number of valid substrings (with exactly k consonants) is equal to atLeastK(word, k) - atLeastK(word, k + 1).
+With this problem reduction, we can simplify our sliding window approach and eliminate the need for an auxiliary data structure to keep track of occurrences of consonants.
  */
