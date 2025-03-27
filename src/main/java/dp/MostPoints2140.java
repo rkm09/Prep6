cd861ea1054e493d641539d8bd92794a4bc7a387
rkm09 <raksha.mishra09@gmail.com>
2025-03-27 20:15:47 +0530

    most points

--- src/main/java/dp/MostPoints2140.java
+++ src/main/java/dp/MostPoints2140.java
@@ -21,9 +21,27 @@ public class MostPoints2140 {
             return dp[index];
         int points = questions[index][0];
         int skip = questions[index][1];
+//        dp[i] = max of (solve it, skip it)
         dp[index] = Math.max(points + dfs(questions, index + skip + 1),
                 dfs(questions, index + 1));
         return dp[index];
+    }
+
+//    bottom up iterative dp; time: O(n), space: O(n) [faster]
+    public long mostPoints1(int[][] questions) {
+        int n = questions.length;
+        dp = new long[n];
+        dp[n - 1] = questions[n - 1][0];
+        for(int i = n - 2 ; i >= 0 ; i--) {
+            dp[i] = questions[i][0];
+            int skip = questions[i][1];
+            if(i + skip + 1 < n) {
+                dp[i] += dp[i + skip + 1];
+            }
+//            dp[i] = max of (solve it, skip it)
+            dp[i] = Math.max(dp[i], dp[i + 1]);
+        }
+        return dp[0];
     }
 }
 
