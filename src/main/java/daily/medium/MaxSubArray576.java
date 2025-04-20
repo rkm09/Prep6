d9808ff603d55cdbf72da3c44a8ae02ac37d8ac0
rkm09 <raksha.mishra09@gmail.com>
2025-04-20 15:49:50 +0530

    max sub array

--- src/main/java/daily/medium/MaxSubArray576.java
+++ src/main/java/daily/medium/MaxSubArray576.java
@@ -15,11 +15,15 @@ public class MaxSubArray576 {
         Map<Integer, Integer> indices = new HashMap<>();
         for(int i = 0 ; i < n ; i++) {
             prefixSum += nums[i];
+//            check if all the numbers seen so far sum to k
             if(prefixSum == k)
                 longestSubArray = i + 1;
+//            if any sub array seen so far sums to k, then update the length of the longest sub array
             if(indices.containsKey(prefixSum - k))
                 longestSubArray = Math.max(longestSubArray, i - indices.get(prefixSum - k));
-            indices.put(prefixSum, i);
+//            only add the current index if the prefix sum is not already present
+            if(!indices.containsKey(prefixSum))
+                indices.put(prefixSum, i);
         }
         return longestSubArray;
     }
@@ -35,6 +39,9 @@ Example 2:
 Input: nums = [-2,-1,2,1], k = 1
 Output: 2
 Explanation: The subarray [-1, 2] sums to 1 and is the longest.
+Example 3:
+Input: nums = [1,0,-1], k = -1
+Output: 2
 Constraints:
 1 <= nums.length <= 2 * 105
 -104 <= nums[i] <= 104
